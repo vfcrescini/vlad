5543c3f8b2213ad314a3e7e47e28319f66bc3f9c
Vino Fernando Crescini <vfcrescini@gmail.com>
2001-06-19 05:14:16 +0000

    initial commit of logger files
    git-svn-id: https://tweety.arinola.net/svn/trunk@17 af43ea2c-c91c-496b-bf95-934215697a66

---
+++ samples/logger/loggerType.cpp
@@ -0,0 +1,97 @@
+/*
+ * loggerType.cpp
+ * Vino Crescini
+ * 18 June 2001
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "loggerType.h"
+
+#define LOGGERTYPE_STRINGINFO    "Logger Info  :"
+#define LOGGERTYPE_STRINGTRACE   "Logger Trace :"
+#define LOGGERTYPE_STRINGERROR   "Logger Error :"
+#define LOGGERTYPE_STRINGHEADER  "---------------------------------------"
+#define LOGGERTYPE_STRINGTRAILER "---------------------------------------"
+
+loggerType::loggerType()
+{
+  mLogLevel = 0;
+  mLogFileStream = NULL;
+}
+
+loggerType::~loggerType()
+{
+  if (mLogFileStream)
+    fclose(mLogFileStream);
+}
+
+bool loggerType::setLevel(int aLevel)
+{
+  if (aLevel >= LOGGERTYPE_LOGNULL && aLevel <= LOGGERTYPE_LOGERROR) {
+    mLogLevel = aLevel;
+    return true;
+  }
+  return false;
+}
+
+bool loggerType::setOutput(char *aFilename)
+{
+  if (!aFilename)
+    mLogFileStream = stderr;
+  else
+    mLogFileStream = fopen(aFilename, "w");
+
+  return (mLogFileStream != NULL);
+}
+
+void loggerType::logInfo(char *aFormat, ...)
+{
+  va_list argPointer;
+  
+  if (aFormat &&
+      mLogLevel >= LOGGERTYPE_LOGINFO &&
+      mLogFileStream) {
+      
+    va_start(argPointer, aFormat);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGHEADER);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGINFO);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGTRAILER);
+    vfprintf(mLogFileStream, aFormat, argPointer);
+    va_end(argPointer);    
+  }
+}
+
+void loggerType::logTrace(char *aFormat, ...)
+{
+  va_list argPointer;
+  
+  if (aFormat &&
+      mLogLevel >= LOGGERTYPE_LOGTRACE &&
+      mLogFileStream) {
+      
+    va_start(argPointer, aFormat);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGHEADER);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGTRACE);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGTRAILER);
+    vfprintf(mLogFileStream, aFormat, argPointer);
+    va_end(argPointer);    
+  }
+}
+
+void loggerType::logError(char *aFormat, ...)
+{
+  va_list argPointer;
+  
+  if (aFormat &&
+      mLogLevel >= LOGGERTYPE_LOGERROR &&
+      mLogFileStream) {
+      
+    va_start(argPointer, aFormat);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGHEADER);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGERROR);
+    fprintf(mLogFileStream, "%s\n", LOGGERTYPE_STRINGTRAILER);
+    vfprintf(mLogFileStream, aFormat, argPointer);
+    va_end(argPointer);    
+  }
+}
---
+++ samples/logger/loggerType.h
@@ -0,0 +1,35 @@
+/*
+ * loggerType.h
+ * Vino Crescini
+ * 18 July 2001
+ */
+
+#ifndef __LOGGERTYPE_H
+#define __LOGGERTYPE_H
+
+#include <stdarg.h>
+
+#define LOGGERTYPE_LOGNULL  0
+#define LOGGERTYPE_LOGINFO  1
+#define LOGGERTYPE_LOGTRACE 2
+#define LOGGERTYPE_LOGERROR 3
+
+class loggerType
+{
+  public:
+    loggerType();
+    ~loggerType();
+
+    bool setLevel(int);
+    bool setOutput(char *);
+    
+    void logInfo(char *, ...);
+    void logTrace(char *, ...);
+    void logError(char *, ...);
+
+  private:
+    int mLogLevel;
+    FILE *mLogFileStream;
+} ;
+
+#endif
