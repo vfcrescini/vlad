b39fe13f29f9916b9026b308937e2a6b05a79d18
Vino Fernando Crescini <vfcrescini@gmail.com>
2002-08-10 15:28:10 +0000

    added ifdef check
    git-svn-id: https://tweety.arinola.net/svn/trunk@146 af43ea2c-c91c-496b-bf95-934215697a66

--- project/epilog/parser/parser.h
+++ project/epilog/parser/parser.h
@@ -3,6 +3,9 @@
  * Vino Crescini  <jcrescin@cit.uws.edu.au>
  */
 
+#ifndef __EPI_PARSER_H
+#define __EPI_PARSER_H
+
 /* vars that can be re-assigned */
 extern FILE *yyin;
 extern FILE *yyout;
@@ -12,3 +15,5 @@ extern int yyparse();
 
 /* function to be called with error */
 int yyerror(char *error);
+
+#endif
