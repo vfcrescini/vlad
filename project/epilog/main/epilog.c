05f6575243f824b113178f16d178e63158fb9ac8
Vino Fernando Crescini <vfcrescini@gmail.com>
2002-10-03 17:36:16 +0000

    added options
    git-svn-id: https://tweety.arinola.net/svn/trunk@276 af43ea2c-c91c-496b-bf95-934215697a66

--- project/epilog/main/epilog.c
+++ project/epilog/main/epilog.c
@@ -4,11 +4,46 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
 #include <parser.h>
+
+#define EPI_VERSION "snapshot-2002-09-18"
 
 int main(int argc, char *argv[])
 {
-  yyin = stdin;
+  int option;
+
+  opterr = 0;
+
+  while ((option = getopt(argc, argv, "vh")) != -1) {
+    switch(option) {
+      case 'v' :
+        fprintf(stdout, "Epilog %s\n", EPI_VERSION);
+        return 0;
+      case 'h' :
+        fprintf(stdout, "Usage: %s [-v|-h|filename]\n", argv[0]);
+        return 0;
+      default :
+        fprintf(stderr, "Usage: %s [-v|-h|filename]\n", argv[0]);
+        return -1;
+    }
+  }
+
+  if (argc == 1 || (argc == 2 && !strcmp(argv[1], "-")))
+    yyin = stdin;
+  else if (argc >= 2) {
+    if ((yyin = fopen(argv[1], "r")) == NULL) {
+      fprintf(stderr, "Cannot open %s for reading\n", argv[1]);
+      return -1;
+    }
+  }
+  else {
+    fprintf(stderr, "Usage: %s [-v|-h|filename]\n", argv[0]);
+    return -1;
+  }
+
   yyout = stdout;
   yyerr = stderr;
 
