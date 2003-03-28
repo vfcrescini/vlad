fc4b0db654b285c2f7597e7fe4bac47213aaef5c
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-03-28 04:21:48 +0000

    added transtab
    git-svn-id: https://tweety.arinola.net/svn/trunk/project/vlad@562 af43ea2c-c91c-496b-bf95-934215697a66

---
+++ kb/transtab.cpp
@@ -0,0 +1,88 @@
+/*
+ * transtab.cpp
+ * Vino Crescini  <jcrescin@cit.uws.edu.au>
+ */
+
+#include <cstdlib>
+#include <cstddef>
+#include <new>
+
+#include <config.h>
+#include <vlad.h>
+#include <transtab.h>
+
+transdef::transdef(char *n, stringlist *v, expression *pr, expression *po)
+{
+  name = n;
+  vtable = v;
+  precond = pr;
+  postcond = po;
+}
+
+transdef::~transdef()
+{
+  if (name != NULL)
+    free(name);
+  if (vtable != NULL)
+    delete vtable;
+  if (precond != NULL)
+    delete precond;
+  if (postcond != NULL)
+    delete postcond;
+}
+
+bool transdef::cmp(list_item *item)
+{
+  transdef *tmp = NULL;
+
+  if (item == NULL)
+    return false;
+
+  if ((tmp = dynamic_cast<transdef *> (item)) == NULL)
+    return false;
+  
+  if (tmp->vtable == NULL && vtable != NULL)
+    return false;
+  else if (tmp->vtable != NULL && vtable == NULL)
+    return false;
+  else if (tmp->vtable != NULL && vtable != NULL && !vtable->cmp(tmp->vtable))
+    return false;
+
+  if (tmp->precond == NULL && precond != NULL)
+    return false;
+  else if (tmp->precond != NULL && precond == NULL)
+    return false;
+  else if (tmp->precond != NULL && precond != NULL && !precond->cmp(tmp->precond))
+    return false;
+
+  if (tmp->postcond == NULL && postcond != NULL)
+    return false;
+  else if (tmp->postcond != NULL && postcond == NULL)
+    return false;
+  else if (tmp->postcond != NULL && postcond != NULL && !postcond->cmp(tmp->postcond))
+    return false;
+
+  return true;
+}
+
+transtab::transtab(const char *n) : list(n, true)
+{
+}
+
+transtab::~transtab()
+{
+  purge(true);
+}
+
+int transtab::add(char *n, stringlist *v, expression *pr, expression *po)
+{
+  transdef *tmp;
+
+  if (n == NULL || v == NULL || pr == NULL || po == NULL)
+    return VLAD_NULLPTR;
+
+  if ((tmp = VLAD_NEW(transdef(n, v, pr, po))) == NULL)
+    return VLAD_MALLOCFAILED;
+
+  return list::add((list_item *) tmp);
+}
