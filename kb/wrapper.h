4556245205b672f0dcb422f998fd77cb30a83a97
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-04-15 18:26:51 +0000

    added wrapper class
    git-svn-id: https://tweety.arinola.net/svn/trunk/project/vlad@658 af43ea2c-c91c-496b-bf95-934215697a66

---
+++ kb/wrapper.h
@@ -0,0 +1,26 @@
+/*
+ * wrapper.h
+ * Vino Crescini  <jcrescin@cit.uws.edu.au>
+ */
+
+#ifndef __VLAD_WRAPPER_H
+#define __VLAD_WRAPPER_H
+
+/* class wrapper for the smodels library */
+
+#include <smodels.h>
+#include <api.h>
+
+class wrapper
+{
+  public :
+    wrapper();
+    ~wrapper();
+    int init();
+  private :
+    Smodels *pr_smod;
+    Api *pr_api;
+    bool initialised;
+} ;
+
+#endif
