b543d8f94a7648df65fd2ec56a56f37e30caa763
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-04-17 05:32:21 +0000

    removed numberlistlist and added purge()
    git-svn-id: https://tweety.arinola.net/svn/trunk/project/vlad@672 af43ea2c-c91c-496b-bf95-934215697a66

--- common/numberlist.h
+++ common/numberlist.h
@@ -23,29 +23,17 @@ class number : public list_item
 } ;
 
 /* a list of numbers */
-class numberlist : public list_item, public list
+class numberlist : public list
 {
   public :
     numberlist();
     ~numberlist();
-    /* compare list with this list */
-    bool cmp(list_item *item);
     /* add a number in the list */
     int add(unsigned int n);
     /* get the i'th number in the list */
     int get(unsigned int i, unsigned int *n);
-} ;
-
-/* a list of number lists */
-class numberlistlist : public list
-{
-  public :
-    numberlistlist();
-    ~numberlistlist();
-    /* add a number list to this list */
-    int add(numberlist *l);
-    /* get the i'th number list in this list */
-    int get(unsigned int i, numberlist **l);
+    /* empty the list */
+    void purge();
 } ;
 
 #endif
