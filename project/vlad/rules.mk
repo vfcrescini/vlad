1cc6785174abdb3f4bc2abee564a1d84c3bf82b0
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-05-06 03:59:59 +0000

    now this time a WORKING safety check
    git-svn-id: https://tweety.arinola.net/svn/trunk@733 af43ea2c-c91c-496b-bf95-934215697a66

--- project/vlad/rules.mk
+++ project/vlad/rules.mk
@@ -3,46 +3,46 @@
 all : copy-includes copy-libs copy-bins
 
 copy-includes : $(INCLUDES)
+ifdef INCLUDES
 	@mkdir -p $(DISTDIR)/include/vlad
-	@if [ "x$(INCLUDES)" != "x" ]; then \
-	for i in $(INCLUDES); do \
+	@for i in $(INCLUDES); do \
 	(cd $(DISTDIR)/include/vlad && $(LN_S) -f ../../../$(CURDIR)/$$i $$i); \
-	done; \
-	fi
+	done
+endif
 
 copy-libs : $(LIBS)
+ifdef LIBS
 	@mkdir -p $(DISTDIR)/lib/vlad
-	@if [ "x$(LIBS)" != "x" ]; then \
-	for i in $(LIBS); do \
+	@for i in $(LIBS); do \
 	(cd $(DISTDIR)/lib/vlad && $(LN_S) -f ../../../$(CURDIR)/$$i $$i); \
-	done; \
-	fi
+	done
+endif
 
 copy-bins : $(BINS)
+ifdef BINS
 	@mkdir -p $(DISTDIR)/bin
-	@if [ "x$(BINS)" != "x" ]; then \
-	for i in $(BINS); do \
+	@for i in $(BINS); do \
 	(cd $(DISTDIR)/bin && $(LN_S) -f ../../$(CURDIR)/$$i $$i); \
-	done; \
-	fi
+	done
+endif
 
 clean :
 	$(RM) $(OBJECTS) $(TEMPS)
 
 distclean : clean
 	$(RM) $(LIBS) $(BINS) Makefile
-	@if [ "x$(INCLUDES)" != "x" ]; then \
-	for i in $(INCLUDES); do \
+ifdef INCLUDES
+	@for i in $(INCLUDES); do \
 	$(RM) $(DISTDIR)/include/vlad/$$i; \
-	done; \
-	fi
-	@if [ "x$(LIBS)" != "x" ]; then \
-	for i in $(LIBS); do \
+	done
+endif
+ifdef LIBS
+	@for i in $(LIBS); do \
 	$(RM) $(DISTDIR)/lib/vlad/$$i; \
-	done; \
-	fi
-	@if [ "x$(BINS)" != "x" ]; then \
-	for i in $(BINS); do \
+	done
+endif
+ifdef BINS
+	@for i in $(BINS); do \
 	$(RM) $(DISTDIR)/bin/$$i; \
-	done; \
-	fi
+	done
+endif
