e299adc9a5fc22eaf52a778dd7d54ad7ac6b1ead
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-05-06 03:51:38 +0000

    added safety checks
    git-svn-id: https://tweety.arinola.net/svn/trunk@732 af43ea2c-c91c-496b-bf95-934215697a66

--- project/vlad/rules.mk
+++ project/vlad/rules.mk
@@ -4,33 +4,45 @@ all : copy-includes copy-libs copy-bins
 
 copy-includes : $(INCLUDES)
 	@mkdir -p $(DISTDIR)/include/vlad
-	@for i in $(INCLUDES); do \
+	@if [ "x$(INCLUDES)" != "x" ]; then \
+	for i in $(INCLUDES); do \
 	(cd $(DISTDIR)/include/vlad && $(LN_S) -f ../../../$(CURDIR)/$$i $$i); \
-	done
+	done; \
+	fi
 
 copy-libs : $(LIBS)
 	@mkdir -p $(DISTDIR)/lib/vlad
-	@for i in $(LIBS); do \
+	@if [ "x$(LIBS)" != "x" ]; then \
+	for i in $(LIBS); do \
 	(cd $(DISTDIR)/lib/vlad && $(LN_S) -f ../../../$(CURDIR)/$$i $$i); \
-	done
+	done; \
+	fi
 
 copy-bins : $(BINS)
 	@mkdir -p $(DISTDIR)/bin
-	@for i in $(BINS); do \
+	@if [ "x$(BINS)" != "x" ]; then \
+	for i in $(BINS); do \
 	(cd $(DISTDIR)/bin && $(LN_S) -f ../../$(CURDIR)/$$i $$i); \
-	done
+	done; \
+	fi
 
 clean :
 	$(RM) $(OBJECTS) $(TEMPS)
 
 distclean : clean
 	$(RM) $(LIBS) $(BINS) Makefile
-	@for i in $(INCLUDES); do \
+	@if [ "x$(INCLUDES)" != "x" ]; then \
+	for i in $(INCLUDES); do \
 	$(RM) $(DISTDIR)/include/vlad/$$i; \
-	done
-	@for i in $(LIBS); do \
+	done; \
+	fi
+	@if [ "x$(LIBS)" != "x" ]; then \
+	for i in $(LIBS); do \
 	$(RM) $(DISTDIR)/lib/vlad/$$i; \
-	done
-	@for i in $(BINS); do \
+	done; \
+	fi
+	@if [ "x$(BINS)" != "x" ]; then \
+	for i in $(BINS); do \
 	$(RM) $(DISTDIR)/bin/$$i; \
-	done
+	done; \
+	fi
