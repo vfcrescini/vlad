f54a3d01fb06e15dae6a6f7d9018b4c768b05af6
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-06-26 03:25:13 +0000

    more cleanup
    git-svn-id: https://tweety.arinola.net/svn/trunk@850 af43ea2c-c91c-496b-bf95-934215697a66

--- project/mod_vlad/admin.c
+++ project/mod_vlad/admin.c
@@ -26,20 +26,6 @@ void modvlad_generate_form(request_rec *a_r, modvlad_config_rec *a_conf)
 {
   unsigned int i;
   unsigned int j;
-  unsigned int st_len;
-  char *st_name;
-  void *st_list;
-  unsigned int tt_len;
-  char *tt_name;
-  void *tt_list;
-  void *tt_prexp;
-  void *tt_poexp;
-  unsigned int arg_len;
-  unsigned int it_len;
-  char *it_name;
-
-  vlad_kb_length_seqtab(a_conf->kb, &st_len);
-  vlad_kb_length_transtab(a_conf->kb, &tt_len);
 
   ap_rprintf(a_r, "    <h3>Current Sequence</h3>\n");
   ap_rprintf(a_r, "    <form name=\"delform\" method=\"POST\" action=\"\">\n");
@@ -47,8 +33,12 @@ void modvlad_generate_form(request_rec *a_r, modvlad_config_rec *a_conf)
   ap_rprintf(a_r, "      <input type=\"hidden\" name=\"delete\" value=\"\">\n");
   ap_rprintf(a_r, "      <ul>\n");
 
-  for (i = 0; i < st_len; i++) {
+  for (i = 0; i < vlad_kb_length_seqtab(a_conf->kb); i++) {
+    char *st_name = NULL;
+    void *st_list = NULL;
+
     vlad_kb_get_seqtab(a_conf->kb, i, &st_name, &st_list);
+
     ap_rprintf(a_r, "         <li>\n");
     ap_rprintf(a_r, "           <input type=\"button\" value=\"delete\" onclick=\"delform.delete.value='%d';delform.submit();\">\n", i);
     ap_rprintf(a_r, "           %s\n", st_name);
@@ -63,10 +53,12 @@ void modvlad_generate_form(request_rec *a_r, modvlad_config_rec *a_conf)
   ap_rprintf(a_r, "      <input type=\"hidden\" name=\"ident\" value=\"\">\n");
   ap_rprintf(a_r, "      <ul>\n");
 
-  vlad_kb_length_symtab(a_conf->kb, &it_len);
 
-  for (i = 0; i < it_len; i++) {
+  for (i = 0; i < vlad_kb_length_symtab(a_conf->kb); i++) {
+    char *it_name = NULL;
+
     vlad_kb_get_symtab(a_conf->kb, i, &it_name);
+
     ap_rprintf(a_r, "        <li>\n");
     ap_rprintf(a_r, "          <input type=\"radio\" onclick=\"idform.ident.value='%s';\">\n", it_name);
     ap_rprintf(a_r, "          %s\n", it_name);
@@ -79,20 +71,24 @@ void modvlad_generate_form(request_rec *a_r, modvlad_config_rec *a_conf)
   ap_rprintf(a_r, "    <h3>Available Transformations</h3>\n");
   ap_rprintf(a_r, "    <ul>\n");
 
-  for (i = 0; i < tt_len; i++) {
+  for (i = 0; i <  vlad_kb_length_transtab(a_conf->kb); i++) {
+    char *tt_name = NULL;
+    void *tt_list = NULL;
+    void *tt_prexp = NULL;
+    void *tt_poexp = NULL;
+
     vlad_kb_get_transtab(a_conf->kb, i, &tt_name, &tt_list, &tt_prexp, &tt_poexp);
-    arg_len = vlad_list_length(tt_list);
 
     ap_rprintf(a_r, "      <li>\n");
     ap_rprintf(a_r, "        <form name=\"addform%d\" method=\"POST\" action=\"\">\n", i);
     ap_rprintf(a_r, "          <input type=\"hidden\" name=\"command\" value=\"add\">\n");
-    ap_rprintf(a_r, "          <input type=\"hidden\" name=\"args\" value=\"%d\">\n", arg_len);
+    ap_rprintf(a_r, "          <input type=\"hidden\" name=\"args\" value=\"%d\">\n",  vlad_list_length(tt_list));
     ap_rprintf(a_r, "          <input type=\"hidden\" name=\"trans\" value=\"%s\">\n", tt_name);
     ap_rprintf(a_r, "          %s\n", tt_name);
     ap_rprintf(a_r, "          <input type=\"button\" name=\"add\" value=\"add\" onclick=\"addform%d.submit();\">\n", i);
     ap_rprintf(a_r, "          <br>\n");
 
-    for (j = 0; j < arg_len; j++) {
+    for (j = 0; j < vlad_list_length(tt_list); j++) {
       ap_rprintf(a_r, "          arg%d\n", j);
       ap_rprintf(a_r, "          <input type=\"text\" name=\"arg%d\" value=\"\" readonly>\n", j);
       ap_rprintf(a_r, "          <input type=\"button\" value=\"set\" onclick=\"addform%d.arg%d.value=document.idform.ident.value;\">\n", i, j);
