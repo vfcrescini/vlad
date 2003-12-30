578530bbad696068b44ca9d93c7b50dd438e1502
Vino Fernando Crescini <vfcrescini@gmail.com>
2003-12-30 17:12:24 +0000

    cleanup
    git-svn-id: https://tweety.arinola.net/svn/trunk@1100 af43ea2c-c91c-496b-bf95-934215697a66

--- project/vlad/lib/stringlist.h
+++ project/vlad/lib/stringlist.h
@@ -14,15 +14,15 @@ class string : public list_item
     string();
     ~string();
     /* compare item with this string */
-    bool cmp(list_item *item);
-    int init(const char *s);
+    bool cmp(list_item *a_item);
+    int init(const char *a_str);
     char *get();
 #ifdef VLAD_DEBUG
     /* assuming s has enough memory allocation */
-    void print(char *s);
+    void print(char *a_str);
 #endif
   private :
-    char *str;
+    char *m_string;
 } ;
 
 class stringlist : public list
@@ -32,16 +32,16 @@ class stringlist : public list
     stringlist();
     ~stringlist();
     /* add a string in the list */
-    int add(const char *s);
+    int add(const char *a_str);
     /* get the index of the string */
-    int get(const char *s, unsigned int *i);
+    int get(const char *a_str, unsigned int *a_index);
     /* get the ith string in the list */
-    int get(unsigned int i, char **s);
+    int get(unsigned int a_index, char **a_str);
     /* return true if string is in the list */
-    int find(const char *s);
+    int find(const char *a_str);
 #ifdef VLAD_DEBUG
     /* assumimg s has enough memory allocation */
-    void print(char *s);
+    void print(char *a_str);
 #endif
 } ;
 
