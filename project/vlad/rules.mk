7fb0d23f2e1102e810bbd030389552869c5e8ed4
Vino Fernando Crescini <vfcrescini@gmail.com>
2004-11-05 17:13:55 +0000

    capitalised vars
    git-svn-id: https://tweety.arinola.net/svn/trunk@1357 af43ea2c-c91c-496b-bf95-934215697a66

--- project/vlad/rules.mk
+++ project/vlad/rules.mk
@@ -1,5 +1,10 @@
 # Vino Fernando Crescini  <jcrescin@cit.uws.edu.au>
 
+define EXEC
+  $(1)
+
+endef
+
 .PHONY : \
   copy-includes \
   copy-static-libs \
@@ -17,11 +22,6 @@
   clean \
   distclean \
   $(NULL)
-
-define exec
-  $(1)
-
-endef
 
 all : \
   copy-includes \
@@ -47,70 +47,70 @@ uninstall : \
 
 copy-includes : $(INCLUDES)
 	mkdir -p $(DISTDIR)/include/vlad
-	$(foreach i,$(INCLUDES),$(call exec,$(LN_S) -f ../../../$(CURDIR)/$(i) $(DISTDIR)/include/vlad/$(i)))
+	$(foreach i,$(INCLUDES),$(call EXEC,$(LN_S) -f ../../../$(CURDIR)/$(i) $(DISTDIR)/include/vlad/$(i)))
 
 copy-static-libs : $(STATIC_LIBS)
 	mkdir -p $(DISTDIR)/lib
-	$(foreach i,$(STATIC_LIBS),$(call exec,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/lib/$(i)))
+	$(foreach i,$(STATIC_LIBS),$(call EXEC,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/lib/$(i)))
 
 ifdef BUILDSHARED
 copy-shared-libs : $(SHARED_LIBS)
 	mkdir -p $(DISTDIR)/lib
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/lib/$(i)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(LN_S) -f $(i) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2,3)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(LN_S) -f $(i) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/lib/$(i)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(LN_S) -f $(i) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2,3)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(LN_S) -f $(i) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2)))
 endif
 
 copy-bins : $(BINS)
 	mkdir -p $(DISTDIR)/bin
-	$(foreach i,$(BINS),$(call exec,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/bin/$(i)))
+	$(foreach i,$(BINS),$(call EXEC,$(LN_S) -f ../../$(CURDIR)/$(i) $(DISTDIR)/bin/$(i)))
 
 install-includes : $(INCLUDES)
 	mkdir -p $(includedir)/vlad
-	$(foreach i,$(INCLUDES),$(call exec,cp -f $(i) $(includedir)/vlad/$(i)))
+	$(foreach i,$(INCLUDES),$(call EXEC,cp -f $(i) $(includedir)/vlad/$(i)))
 
 install-static-libs : $(STATIC_LIBS)
 	mkdir -p $(libdir)
-	$(foreach i,$(STATIC_LIBS),$(call exec,cp -f $(i) $(libdir)/$(i)))
+	$(foreach i,$(STATIC_LIBS),$(call EXEC,cp -f $(i) $(libdir)/$(i)))
 
 ifdef BUILDSHARED
 install-shared-libs : $(SHARED_LIBS)
 	mkdir -p $(libdir)
-	$(foreach i,$(SHARED_LIBS),$(call exec,cp -f $(i) $(libdir)/$(i)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(LN_S) -f $(i) $(libdir)/$(shell echo $(i) | cut -d. -f1,2,3)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(LN_S) -f $(i) $(libdir)/$(shell echo $(i) | cut -d. -f1,2)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,cp -f $(i) $(libdir)/$(i)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(LN_S) -f $(i) $(libdir)/$(shell echo $(i) | cut -d. -f1,2,3)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(LN_S) -f $(i) $(libdir)/$(shell echo $(i) | cut -d. -f1,2)))
 endif
 
 install-bins : $(BINS)
 	mkdir -p $(bindir)
-	$(foreach i,$(BINS),$(call exec,cp -f $(i) $(bindir)/$(i)))
+	$(foreach i,$(BINS),$(call EXEC,cp -f $(i) $(bindir)/$(i)))
 
 uninstall-includes :
-	$(foreach i,$(INCLUDES),$(call exec,$(RM) $(includedir)/vlad/$(i)))
+	$(foreach i,$(INCLUDES),$(call EXEC,$(RM) $(includedir)/vlad/$(i)))
 	$(RM) -r $(includedir)/vlad
 
 uninstall-static-libs :
-	$(foreach i,$(STATIC_LIBS),$(call exec,$(RM) $(libdir)/$(i)))
+	$(foreach i,$(STATIC_LIBS),$(call EXEC,$(RM) $(libdir)/$(i)))
 
 ifdef BUILDSHARED
 uninstall-shared-libs :
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(libdir)/$(i)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(libdir)/$(shell echo $(i) | cut -d. -f1,2,3)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(libdir)/$(shell echo $(i) | cut -d. -f1,2)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(libdir)/$(i)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(libdir)/$(shell echo $(i) | cut -d. -f1,2,3)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(libdir)/$(shell echo $(i) | cut -d. -f1,2)))
 endif
 
 uninstall-bins :
-	$(foreach i,$(BINS),$(call exec,$(RM) $(bindir)/$(i)))
+	$(foreach i,$(BINS),$(call EXEC,$(RM) $(bindir)/$(i)))
 
 clean :
-	$(foreach i,$(TEMPS1) $(OBJECTS),$(call exec,$(RM) $(i)))
+	$(foreach i,$(TEMPS1) $(OBJECTS),$(call EXEC,$(RM) $(i)))
 
 distclean : clean
-	$(foreach i,$(INCLUDES),$(call exec,$(RM) $(DISTDIR)/include/vlad/$(i)))
-	$(foreach i,$(STATIC_LIBS),$(call exec,$(RM) $(DISTDIR)/lib/$(i)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(DISTDIR)/lib/$(i)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2,3)))
-	$(foreach i,$(SHARED_LIBS),$(call exec,$(RM) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2)))
-	$(foreach i,$(BINS),$(call exec,$(RM) $(DISTDIR)/bin/$(i)))
+	$(foreach i,$(INCLUDES),$(call EXEC,$(RM) $(DISTDIR)/include/vlad/$(i)))
+	$(foreach i,$(STATIC_LIBS),$(call EXEC,$(RM) $(DISTDIR)/lib/$(i)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(DISTDIR)/lib/$(i)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2,3)))
+	$(foreach i,$(SHARED_LIBS),$(call EXEC,$(RM) $(DISTDIR)/lib/$(shell echo $(i) | cut -d. -f1,2)))
+	$(foreach i,$(BINS),$(call EXEC,$(RM) $(DISTDIR)/bin/$(i)))
 	$(RM) -r $(DISTDIR)/include/vlad
 	$(RM) $(TEMPS2) $(STATIC_LIBS) $(SHARED_LIBS) $(BINS) Makefile
