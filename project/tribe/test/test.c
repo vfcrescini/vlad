7615bea4d34b6390c414fe273f1166afa35d6074
Vino Fernando Crescini <vfcrescini@gmail.com>
2005-02-24 05:15:32 +0000

    added an inconsistent case
    git-svn-id: https://tweety.arinola.net/svn/trunk@1460 af43ea2c-c91c-496b-bf95-934215697a66

--- project/tribe/test/test.c
+++ project/tribe/test/test.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
   unsigned int rs;
   tbe_net net;
-
+#if 0
   tbe_net_init(&net);
   tbe_net_add_int(&net, 1);
   tbe_net_add_int(&net, 2);
@@ -46,6 +46,98 @@ int main(int argc, char *argv[])
 
   tbe_net_dump2(net, stdout);
   tbe_net_purge(&net);
+#endif
+
+  tbe_net_init(&net);
+  tbe_net_add_int(&net, 1);
+  tbe_net_add_int(&net, 2);
+  tbe_net_add_int(&net, 3);
+  tbe_net_add_int(&net, 4);
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_STA);
+  TBE_REL_SET_ADD(rs, TBE_REL_MET);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 4, 1, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUR);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUI);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 1, 2, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_OVR);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 4, 2, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUR);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUI);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 2, 3, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_STA);
+  TBE_REL_SET_ADD(rs, TBE_REL_MET);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 4, 3, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_FIN);
+  TBE_REL_SET_ADD(rs, TBE_REL_FII);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 1, 3, rs));
+
+  tbe_net_dump2(net, stdout);
+  tbe_net_purge(&net);
+
+
+#if 0
+  tbe_net_init(&net);
+  tbe_net_add_int(&net, 1);
+  tbe_net_add_int(&net, 2);
+  tbe_net_add_int(&net, 3);
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_BEF);
+  TBE_REL_SET_ADD(rs, TBE_REL_BEI);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 1, 2, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_BEF);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 2, 3, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_BEF);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 3, 1, rs));
+
+  tbe_net_dump2(net, stdout);
+  tbe_net_purge(&net);
+#endif
+#if 0
+  tbe_net_init(&net);
+  tbe_net_add_int(&net, 1);
+  tbe_net_add_int(&net, 2);
+  tbe_net_add_int(&net, 3);
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUR);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 1, 2, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUI);
+
+  printf("adding: %d\n", tbe_net_add_rel(&net, 2, 3, rs));
+
+
+  tbe_net_dump2(net, stdout);
+  tbe_net_purge(&net);
+#endif
 
   return 0;
 }
