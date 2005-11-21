9af5fa054b1ddcc88e12f54d4bcd9052137d7664
Vino Fernando Crescini <vfcrescini@gmail.com>
2005-11-21 16:42:56 +0000

    renamed all test intervals so that they start with 0
    git-svn-id: https://tweety.arinola.net/svn/trunk@1631 af43ea2c-c91c-496b-bf95-934215697a66

--- project/tribe/test/test.c
+++ project/tribe/test/test.c
@@ -33,10 +33,10 @@ int main(int argc, char *argv[])
 
 #if 0
   tbe_net_create(&net);
+  tbe_net_add_interval(net, 0);
   tbe_net_add_interval(net, 1);
   tbe_net_add_interval(net, 2);
   tbe_net_add_interval(net, 3);
-  tbe_net_add_interval(net, 4);
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_BEF);
@@ -44,46 +44,46 @@ int main(int argc, char *argv[])
   TBE_REL_SET_ADD(rs, TBE_REL_MEI);
   TBE_REL_SET_ADD(rs, TBE_REL_BEI);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 1, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_OVR);
   TBE_REL_SET_ADD(rs, TBE_REL_MET);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 2, 3, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_OVR);
   TBE_REL_SET_ADD(rs, TBE_REL_STA);
   TBE_REL_SET_ADD(rs, TBE_REL_DUR);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 3, 1, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 2, 0, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_DUR);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 4, 2, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 3, 1, rs));
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, TBE_REL_SET_ALL));
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 1, TBE_REL_SET_ALL));
 
   tbe_net_dump2(net, stdout);
 
   TBE_INTERVAL_INIT(i, 10, 20, TBE_INTERVAL_EP_ALL);
-  printf("adding: %d\n", tbe_net_add_endpoints(net, 1, i));
+  printf("adding: %d\n", tbe_net_add_endpoints(net, 0, i));
 
   TBE_INTERVAL_INIT(i, 0,  4, TBE_INTERVAL_EP_ALL);
-  printf("adding: %d\n", tbe_net_add_endpoints(net, 2, i));
+  printf("adding: %d\n", tbe_net_add_endpoints(net, 1, i));
 
   tbe_net_dump2(net, stdout);
 
-  printf("interval 2 to [3,15]: ");
+  printf("interval 1 to [3,15]: ");
   TBE_INTERVAL_INIT(i, 3, 15, TBE_INTERVAL_EP_ALL);
-  tbe_rel_dump(tbe_net_get_relation2(net, 2, i), stdout);
+  tbe_rel_dump(tbe_net_get_relation2(net, 1, i), stdout);
   printf("\n");
 
-  printf("interval 2 to [0,5]: ");
+  printf("interval 1 to [0,5]: ");
   TBE_INTERVAL_INIT(j, 0, 5, TBE_INTERVAL_EP_ALL);
-  tbe_rel_dump(tbe_net_get_relation2(net, 2, j), stdout);
+  tbe_rel_dump(tbe_net_get_relation2(net, 1, j), stdout);
   printf("\n");
 
   tbe_net_dump2(net, stdout);
@@ -93,16 +93,27 @@ int main(int argc, char *argv[])
 
 #if 1
   tbe_net_create(&net);
+  tbe_net_add_interval(net, 0);
   tbe_net_add_interval(net, 1);
   tbe_net_add_interval(net, 2);
   tbe_net_add_interval(net, 3);
-  tbe_net_add_interval(net, 4);
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_STA);
   TBE_REL_SET_ADD(rs, TBE_REL_MET);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 4, 1, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 3, 0, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUR);
+  TBE_REL_SET_ADD(rs, TBE_REL_DUI);
+
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 1, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_OVR);
+
+  printf("adding: %d\n", tbe_net_add_relation(net, 3, 1, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_DUR);
@@ -111,27 +122,16 @@ int main(int argc, char *argv[])
   printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
 
   TBE_REL_SET_CLEAR(rs);
-  TBE_REL_SET_ADD(rs, TBE_REL_OVR);
-
-  printf("adding: %d\n", tbe_net_add_relation(net, 4, 2, rs));
-
-  TBE_REL_SET_CLEAR(rs);
-  TBE_REL_SET_ADD(rs, TBE_REL_DUR);
-  TBE_REL_SET_ADD(rs, TBE_REL_DUI);
-
-  printf("adding: %d\n", tbe_net_add_relation(net, 2, 3, rs));
-
-  TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_STA);
   TBE_REL_SET_ADD(rs, TBE_REL_MET);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 4, 3, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 3, 2, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_FIN);
   TBE_REL_SET_ADD(rs, TBE_REL_FII);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 1, 3, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 2, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_DUR);
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
   TBE_REL_SET_ADD(rs, TBE_REL_FIN);
   TBE_REL_SET_ADD(rs, TBE_REL_FII);
 
-  tbe_net_get_intervals(net, 1, rs, dump, (void *) stdout);
+  tbe_net_get_intervals(net, 0, rs, dump, (void *) stdout);
 
   tbe_net_dump2(net, stdout);
   tbe_net_destroy(&net);
@@ -207,25 +207,25 @@ int main(int argc, char *argv[])
 
 #if 0
   tbe_net_create(&net);
+  tbe_net_add_interval(net, 0);
   tbe_net_add_interval(net, 1);
   tbe_net_add_interval(net, 2);
-  tbe_net_add_interval(net, 3);
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_BEF);
   TBE_REL_SET_ADD(rs, TBE_REL_BEI);
+
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 1, rs));
+
+  TBE_REL_SET_CLEAR(rs);
+  TBE_REL_SET_ADD(rs, TBE_REL_BEF);
 
   printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_BEF);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 2, 3, rs));
-
-  TBE_REL_SET_CLEAR(rs);
-  TBE_REL_SET_ADD(rs, TBE_REL_BEF);
-
-  printf("adding: %d\n", tbe_net_add_relation(net, 3, 1, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 2, 0, rs));
 
   tbe_net_dump2(net, stdout);
   tbe_net_destroy(&net);
@@ -233,19 +233,19 @@ int main(int argc, char *argv[])
 
 #if 0
   tbe_net_create(&net);
+  tbe_net_add_interval(net, 0);
   tbe_net_add_interval(net, 1);
   tbe_net_add_interval(net, 2);
-  tbe_net_add_interval(net, 3);
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_DUR);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 0, 1, rs));
 
   TBE_REL_SET_CLEAR(rs);
   TBE_REL_SET_ADD(rs, TBE_REL_DUI);
 
-  printf("adding: %d\n", tbe_net_add_relation(net, 2, 3, rs));
+  printf("adding: %d\n", tbe_net_add_relation(net, 1, 2, rs));
 
   tbe_net_dump2(net, stdout);
   tbe_net_destroy(&net);
