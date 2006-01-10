ee2b605865b0c29c6f657cd5f940d05f4fcff30e
Vino Fernando Crescini <vfcrescini@gmail.com>
2006-01-10 19:10:36 +0000

    unsigned char to unsigned int
    git-svn-id: https://tweety.arinola.net/svn/branches/vlad-1_4-br/project/vlad@1830 af43ea2c-c91c-496b-bf95-934215697a66

--- lib/mapper.cpp
+++ lib/mapper.cpp
@@ -152,8 +152,8 @@ unsigned int vlad_mapper::compute_memb(unsigned char a_type,
 
 /* given the state, truth value and indices, map a subst fact into an id */
 unsigned int vlad_mapper::compute_subst(unsigned char a_type,
-                                        unsigned char a_grp1,
-                                        unsigned char a_grp2,
+                                        unsigned int a_grp1,
+                                        unsigned int a_grp2,
                                         unsigned int a_state,
                                         bool a_truth)
 {
